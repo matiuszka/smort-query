54c0114e5a71293d4a35c0e2913df1b60387802f
Mateusz Nowak <mateusz.nowak@zf.com>
2020-04-07 18:32:04 +0200

    License changed

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = "1.0.0"
+version = "1.0.1"
 
 with open("README.md") as readme_file:
     readme = readme_file.read()
@@ -30,6 +30,6 @@ setup(
     package_dir={"smort_query": "smort_query"},
     include_package_data=True,
     install_requires=requirements,
-    license="GPLv3",
+    license="MIT",
     zip_safe=False,
 )
